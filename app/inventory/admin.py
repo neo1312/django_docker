c6e94b7e6568da37c8c2a969cedf8d369646dc26
leonvic13 <leonvic13@gmail.com>
2025-06-05 13:27:23 -0600

    feat: configure Product in admin with advanced options

--- app/inventory/admin.py
+++ app/inventory/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'last_updated')
+    list_editable = ('price', 'quantity')
+    search_fields = ('name',)
+    list_filter = ('created_at',)
+    date_hierarchy = 'created_at'
+
+    @admin.display(description='Last Updated')
+    def last_updated(self, obj):
+        return obj.updated_at.strftime("%Y-%m-%d %H:%M")
